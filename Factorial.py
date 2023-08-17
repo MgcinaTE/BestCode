606c6dc8b802174fd1f8c77c39a9b411f106dc29
MgcinaTE <te.mgcina#gmail.com>
2023-08-18 00:02:46 +0200

    Add factorial calculation code

--- Factorial.py
+++ Factorial.py
@@ -1,3 +1,6 @@
+#This code calculates a factorial of a any positive number 
+#The user is asked to enter a number then a factorial of that number is given back as an answer
+
 def factorial(n):
     result = 1
     for i in range(1, n + 1):
